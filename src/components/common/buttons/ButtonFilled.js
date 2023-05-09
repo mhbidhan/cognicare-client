e6e1a5bc67a5c05f348fa73765b0a068aa3e3e56
mubtasim <mubtasim91@gmail.com>
2023-05-10 00:42:42 +0600

    removed unecessary codes from ButtonFilled

--- src/components/common/buttons/ButtonFilled.js
+++ src/components/common/buttons/ButtonFilled.js
@@ -1,55 +1,10 @@
 import React from 'react';
-import { Text, TouchableHighlight, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import globalStyles from '../../../utils/globalStyle';
 
-function ButtonFilled({
-  text,
-  onPressHandler,
-  // width,
-  // height,
-  // textSize,
-  // btnColor,
-  icon,
-}) {
+function ButtonFilled({ text, onPressHandler, icon }) {
   return (
-    // <TouchableHighlight
-    //   style={{
-    //     width: width ? width : 328,
-    //     height: height ? height : 50,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     padding: 10,
-    //     borderRadius: 10,
-    //     backgroundColor: btnColor ? btnColor : globalStyles.colors.primary,
-    //   }}
-    //   onPress={onPressHandler}
-    //   underlayColor={btnColor ? btnColor : globalStyles.colors.primary}
-    // >
-    //   <View
-    //     style={{
-    //       shadowColor: 'white',
-    //       shadowOffset: {
-    //         width: 0,
-    //         height: 0,
-    //       },
-    //       shadowOpacity: 1,
-    //       shadowRadius: 10,
-    //     }}
-    //   >
-    //     <Text
-    //       style={{
-    //         color: '#fff',
-    //         textAlign: 'center',
-    //         fontSize: textSize ? textSize : 16,
-    //       }}
-    //     >
-    //       {text}
-    //     </Text>
-    //   </View>
-    // </TouchableHighlight>
     <Button
-      // icon='account-eye'
       icon={icon ? icon : null}
       mode='elevated'
       buttonColor={globalStyles.colors.primary}
