40093e1378671274ecc93deece9a1497a6f7ae99
mubtasim <mubtasim91@gmail.com>
2023-05-16 00:28:41 +0600

    added titles to sahha blood input

--- src/components/SahhaLogForm/SahhaLogForm.js
+++ src/components/SahhaLogForm/SahhaLogForm.js
@@ -1,59 +1,92 @@
 import React, { useState } from 'react';
 import { Dimensions, View } from 'react-native';
-import { Button, ToggleButton, TextInput } from 'react-native-paper';
+import { Button, ToggleButton, TextInput, Text } from 'react-native-paper';
 import DropDownPicker from 'react-native-dropdown-picker';
 import ButtonFilled from '../common/buttons/ButtonFilled';
 import globalStyles from '../../utils/globalStyle';
 
 const BloodInput = () => {
-  const [glucoseOpen, setGlucoseOpen] = useState(false);
-  const [glucoseValue, setGlucoseValue] = useState(null);
-  const [glucoseItems, setGlucoseItems] = useState([
+  const [glucoseDropdownOpen, setGlucoseDropdownOpen] = useState(false);
+  const [glucoseDropdownValue, setGlucoseDropdownValue] = useState(null);
+  const [glucoseDropdownItems, setGlucoseDropdownItems] = useState([
     { label: 'Before Meal', value: 'Before Meal' },
     { label: 'After Meal', value: 'After Meal' },
   ]);
+  const [glucoseMgdl, setGlucoseMgdl] = useState('');
 
-  const [pressureOpen, setPressureOpen] = useState(false);
-  const [pressureValue, setPressureValue] = useState(null);
-  const [pressureItems, setPressureItems] = useState([
+  const [pressureDropdownOpen, setPressureDropdownOpen] = useState(false);
+  const [pressureDropdownValue, setPressureDropdownValue] = useState(null);
+  const [pressureDropdownItems, setPressureDropdownItems] = useState([
     { label: 'Before Meal', value: 'Before Meal' },
     { label: 'After Meal', value: 'After Meal' },
   ]);
+  const [pressureSys, setPressureSys] = useState('');
+  const [pressureDias, setPressureDias] = useState('');
+
+  const consoleBloodStates = () => {
+    console.log(glucoseDropdownValue);
+    console.log(glucoseMgdl);
+    console.log(pressureDropdownValue);
+    console.log(pressureSys);
+    console.log(pressureDias);
+  };
 
   const onBloodSubmit = async () => {
     try {
+      consoleBloodStates();
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <View>
+    <View style={{ rowGap: 20 }}>
       <View style={{ zIndex: 1000 }}>
-        <TextInput label='Blood Glucose (mg/dL)' />
+        <Text variant='titleLarge' style={{ color: 'white' }}>
+          Blood Glucose
+        </Text>
+        <TextInput
+          label='Count (mg/dL)'
+          value={glucoseMgdl}
+          onChangeText={setGlucoseMgdl}
+          keyboardType='numeric'
+        />
         <View>
           <DropDownPicker
-            open={glucoseOpen}
-            value={glucoseValue}
-            items={glucoseItems}
-            setOpen={setGlucoseOpen}
-            setValue={setGlucoseValue}
-            setItems={setGlucoseItems}
+            open={glucoseDropdownOpen}
+            value={glucoseDropdownValue}
+            items={glucoseDropdownItems}
+            setOpen={setGlucoseDropdownOpen}
+            setValue={setGlucoseDropdownValue}
+            setItems={setGlucoseDropdownItems}
             placeholder='Relation To Meal'
           />
         </View>
       </View>
       <View style={{ zIndex: 900 }}>
-        <TextInput label='Blood Pressure (Systolic)' />
-        <TextInput label='Blood Pressure (Diastolic)' />
+        <Text variant='titleLarge' style={{ color: 'white' }}>
+          Blood Pressure
+        </Text>
+        <TextInput
+          label='Count (Systolic)'
+          value={pressureSys}
+          onChangeText={setPressureSys}
+          keyboardType='numeric'
+        />
+        <TextInput
+          label='Count (Diastolic)'
+          value={pressureDias}
+          onChangeText={setPressureDias}
+          keyboardType='numeric'
+        />
         <View>
           <DropDownPicker
-            open={pressureOpen}
-            value={pressureValue}
-            items={pressureItems}
-            setOpen={setPressureOpen}
-            setValue={setPressureValue}
-            setItems={setPressureItems}
+            open={pressureDropdownOpen}
+            value={pressureDropdownValue}
+            items={pressureDropdownItems}
+            setOpen={setPressureDropdownOpen}
+            setValue={setPressureDropdownValue}
+            setItems={setPressureDropdownItems}
             placeholder='Relation To Meal'
           />
         </View>
